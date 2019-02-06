aca5f4ad4bec199bcbaea967187a66adbf69c88c
Rafael Dantas <dantasrafael@yahoo.com.br>
2019-02-05 23:52:57 -0200

    Obtendo um usuário por id

--- src/main/java/com/rafaeldantas/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/rafaeldantas/workshopmongodb/resources/UserResource.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -21,9 +22,13 @@ public class UserResource {
 
 	@GetMapping
 	public ResponseEntity<List<UserDTO>> findAll() {
-		List<UserDTO> listaDTO = service.findAll().stream()
-				.map(x -> new UserDTO(x)).collect(Collectors.toList()); 
+		List<UserDTO> listaDTO = service.findAll().stream().map(x -> new UserDTO(x)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listaDTO);
+	}
+
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+		return ResponseEntity.ok().body(new UserDTO(service.findById(id)));
 	}
 
 }
--- src/main/java/com/rafaeldantas/workshopmongodb/services/UserService.java
+++ src/main/java/com/rafaeldantas/workshopmongodb/services/UserService.java
@@ -1,12 +1,14 @@
 package com.rafaeldantas.workshopmongodb.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.rafaeldantas.workshopmongodb.domain.User;
 import com.rafaeldantas.workshopmongodb.repository.UserRepository;
+import com.rafaeldantas.workshopmongodb.services.exception.ObjectNotFoundException;
 
 @Service
 public class UserService {
@@ -17,5 +19,11 @@ public class UserService {
 	public List<User> findAll() {
 		return repo.findAll();
 	}
-	
+
+	public User findById(String id) {
+		Optional<User> user = repo.findById(id);
+		return user.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + User.class.getName()));
+	}
+
 }
